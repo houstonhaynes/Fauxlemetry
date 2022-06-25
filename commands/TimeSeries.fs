2f3492b683f85054d913cbe60fcd7725b6fe52a1
Houston Haynes <h3techsme@gmail.com>
2022-06-24 23:47:12 -0400

    Weird cannot get commands to parse

--- commands/TimeSeries.fs
+++ commands/TimeSeries.fs
@@ -1,18 +1,37 @@
 namespace Commands
 
 module TimeSeries =
+    open System
+    open System.Threading
     open Spectre.Console.Cli
     open Output
 
-    type TimeSeriesSettings() =
+    type EmitSettings() =
+        inherit CommandSettings()
+
+        [<CommandOption("-e|--emit")>]
+        member val emitInterval = 1000 with get, set
+
+    type GenerateSettings() =
         inherit CommandSettings()
 
         [<CommandOption("-d|--days")>]
-        member val days = 30 with get, set
-    
+        member val days = 30 with get, set    
+
+    type Emit() =
+        inherit Command<EmitSettings>()
+        interface ICommandLimiter<EmitSettings>
+
+         override _.Execute(_context, settings) =
+            for i in 1..5 do
+                let dateTime = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff")
+                printMarkedUp $"{ dateTime } :: Data will emit every {emphasize settings.emitInterval} milliseconds" 
+                Thread.Sleep(settings.emitInterval)
+            0
+
     type Generate() =
-        inherit Command<TimeSeriesSettings>()
-        interface ICommandLimiter<TimeSeriesSettings>
+        inherit Command<GenerateSettings>()
+        interface ICommandLimiter<GenerateSettings>
 
          override _.Execute(_context, settings) =
             printMarkedUp $"You've set data to run for {emphasize settings.days} days!"
